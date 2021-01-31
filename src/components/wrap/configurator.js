73aecc439ed14c30b6b319b0265e70d5d2d265cf
sariHilman <sarih@leader.codes>
2021-01-31 13:28:46 +0200

    add switch to configurator

--- src/components/wrap/configurator.js
+++ src/components/wrap/configurator.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { connect } from "react-redux";
+
 import './wrap-component.css'
+import Home from "../home";
+import OpenStore from "../openStore";
+
 
 
 function Configurator(props) {
@@ -8,8 +13,35 @@ function Configurator(props) {
             <div className="MuiDrawer-paperAnchorRight MuiDrawer-paperAnchorDockedRight MuiDrawer-paper Configurator-drawerPaper-50">
                 <h3>למקם פה קונפיגורטור</h3>
 
+                {
+                    props.logoDesign.currentComponent ? (() => {
+                        switch (props.logoDesign.currentComponent) {
+                            case "Home": return <Home />;
+                            case "OpenStore": return <OpenStore />;
+                        }
+                    })() :
+                        <h3>didnt check</h3>
+                }
+
+
+
+
+
             </div>
         </div>
     )
 }
-export default Configurator;
+
+
+
+const mapStateToProps = (state) => {
+    return {
+        logoDesign: state.logoReducer.logoDesign,
+
+    }
+}
+const mapDispatchToProps = (dispatch) => ({
+
+})
+export default connect(mapStateToProps, mapDispatchToProps)(Configurator);
+
