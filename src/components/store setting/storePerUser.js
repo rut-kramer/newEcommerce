8fc611d0e121d14b19e9507b8c5af497f70e0824
rina-mazoz <rina@leader.codes>
2021-02-02 14:18:37 +0200

    delete

--- src/components/store setting/storePerUser.js
+++ src/components/store setting/storePerUser.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { actions } from '../../redux/action'
 import categoryReducer from '../../redux/reducers/data_reducer/categoryReducer';
-import $ from 'jquery';
 
 function StorePerUser(props) {
 
