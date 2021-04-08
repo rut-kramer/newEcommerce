4369b42f37bb263f4e96161cd02c007811aa2c92
tehilaz <tehilaz@leader.codes>
2021-04-08 11:15:48 +0300

    background image of upload image

--- src/components/store design/media_gallery/uploud_images/uploadImages.js
+++ src/components/store design/media_gallery/uploud_images/uploadImages.js
@@ -3,7 +3,7 @@ import './uploadImages.css'
 import { actions } from "../../../../redux/action"
 import { connect } from 'react-redux';
 import uploadImg from '../../../../assets/uploadImg.svg'
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function UploadImages(props) {
     const [fileToUpload, setFileToUpload] = useState(null);
@@ -28,11 +28,16 @@ function UploadImages(props) {
     // }
     return (
         <>
-            <div className="uploud-image">
+            <div className="uploud-image"
+                style={{ myImage } ? {
+                    backgroundImage: `url(${myImage})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat'
+                } : { display: "none" }}>
                 <div className="uploud-image-middle">
-                    <label for="files"></label>  
-                   <img className="myImage" src={myImage} style={myImage ? {} : {display: "none"}}></img>
-                   <div> <img className="upload-img" src={uploadImg} style={myImage ? {display: "none"} : {}} alt="uploadImg" /></div>
+                    <label for="files"></label>
+                    <div> <img className="upload-img" src={uploadImg} style={myImage ? { display: "none" } : {}} alt="uploadImg" /></div>
                     <div><p className="upload-image-word mt-2 mb-1">Drop Image Here</p></div>
                     <div><p className="upload-image-word pl-5 ml-3 mt-0">Or</p></div>
                     <div className="uploud-image-choose-file-div ml-2">
@@ -51,7 +56,6 @@ function UploadImages(props) {
             </div>
         </>
     )
-
 }
 const mapStateToProps = (state) => {
     return {
